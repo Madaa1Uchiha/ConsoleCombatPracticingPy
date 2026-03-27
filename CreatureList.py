bf3d991b3f8b96d7d73cb4ae7d79c962fdaf0ae3
SamMacciocca <1033512@student.sae.edu.au>
2026-03-27 16:51:13 +1100

    added an attack to each will add levels with stats increasing later

--- CreatureList.py
+++ CreatureList.py
@@ -6,16 +6,29 @@ from Attacks import Attacks
 class CreatureList: 
     def __init__(self):
         self.creatures = []
-        self.creatures.append(CreatureInfo("A small, black horse with a white spot on its forehead, almost as if risen from the ashes giving it its fire type and name Ash steed.", "Ash Steed", 100, 2, 25, 10, "Fire"))
-        self.creatures.append(CreatureInfo("A statured Bird a king of the water spearing fishes with its long beak, because of its time around water it has taken the typing of water over generations.", "Water Javelin", 50 , 3.2, 30, 15, "Water"))
-        self.creatures.append(CreatureInfo("This creature that seems to be made of wood like a doll is actually a puppet to a creature of living vines giving it a grass type and the name of Wormvine.", "Wormvine", 80, 1.8, 15, 28, "Grass"))
+        self.creatures.append(CreatureInfo("A small, black horse with a white spot on its forehead, almost as if risen from the ashes giving it its fire type and name Ash steed.", "Ash Steed", 100, 5, 25, 10, "Fire"))
+        self.creatures.append(CreatureInfo("A statured Bird a king of the water spearing fishes with its long beak, because of its time around water it has taken the typing of water over generations.", "Water Javelin", 50 , 7, 30, 15, "Water"))
+        self.creatures.append(CreatureInfo("This creature that seems to be made of wood like a doll is actually a puppet to a creature of living vines giving it a grass type and the name of Wormvine.", "Wormvine", 80, 3.8, 15, 28, "Grass"))
+       
+       
+       
+       
         ashsteed_attack1 = Attacks("Flame Charge", "Fire", 5)
         ashsteed_attack2 = Attacks("Ember", "Fire", 5.8)
-        self.creatures[0].attacks.extend([ashsteed_attack1, ashsteed_attack2])
+        ashsteed_attack3 = Attacks("Tackle", "Normal", 4.5)
+        self.creatures[0].attacks.extend([ashsteed_attack1, ashsteed_attack2, ashsteed_attack3])
+        
+        
+      
         waterjavelin_attack1 = Attacks("Water Gun", "Water", 6)
         waterjavelin_attack2 = Attacks("Aqua Tail", "Water", 4.3)
-        self.creatures[1].attacks.extend([waterjavelin_attack1, waterjavelin_attack2])
+        waterjavelin_attack3 = Attacks("Peck", "Normal", 3.5)
+        self.creatures[1].attacks.extend([waterjavelin_attack1, waterjavelin_attack2, waterjavelin_attack3])
+        
+        
+        
         wormvine_attack1 = Attacks("Vine Whip", "Grass", 3)
         wormvine_attack2 = Attacks("Razor Leaf", "Grass", 4.2)   
-        self.creatures[2].attacks.extend([wormvine_attack1, wormvine_attack2])
+        wormvine_attack3 = Attacks("Tackle", "Normal", 4.5)
+        self.creatures[2].attacks.extend([wormvine_attack1, wormvine_attack2, wormvine_attack3])
 
--- Program.py
+++ Program.py
@@ -8,12 +8,13 @@ import random
 class Program():
 
     def main():
+
+
         players = Player()
         enemy = Enemy()
         dmg = Dmg_Heal()
-        battle = False
         creature_list = CreatureList()
-
+   
     #Just a welcome message and asking for the player's name and gender
         print ("Welcome to Console Combat Practicing Py!")
         print ("This like pokemon where you get to choose a creature to fight with and battle against an enemy, but with a twist of you being able to choose your name and creature!")
@@ -22,15 +23,29 @@ class Program():
         print("Hello " + players.name + " the " + players.gender + "!")
 
     #Asking the player to choose a creature and showing them the list of creatures to choose from
-        print ("Here are the creatures you can choose from: ")
+       # print ("Here are the creatures you can choose from: ")
 
-        for creature in creature_list.creatures:
-            print(creature.name + ": " + creature.description)
+       # for creature in creature_list.creatures:
+         #  print(creature.name + ": " + creature.description)
        
-        choice = input("Which would you like to choose? Enter the name of the creature: ")
-        players.creature = next((c for c in creature_list.creatures if c.name.lower() == choice.lower()), None)
-        if players.creature is None:
-            print("Creature not found.")
+
+        creature_choice = creature_list.creatures
+        for i, creature in enumerate(creature_choice):
+            print(str(i + 1) + ". " + creature.name)
+
+        creature_choice = input("Choose a creature by number or name: ")
+        if creature_choice.isdigit():
+            index = int(creature_choice) - 1
+            if 0 <= index < len(creature_list.creatures):
+                players.creature = creature_list.creatures[index]
+            else:
+                print("Invalid number.")
+                players.creature = None
+        else:
+            players.creature = next((c for c in creature_list.creatures if c.name.lower() == creature_choice.lower()), None)
+            if players.creature is None:
+                print("Creature not found.")
+            
 
         print("You chose " + players.creature.name + "!")
         print("Your creature's stats are: Health: " + str(players.creature.health) + ", Attack Power: " + str(players.creature.attackPower) + ", Speed: " + str(players.creature.speed) + ", Defense: " + str(players.creature.defense) + ", Type: " + players.creature.type)
@@ -38,17 +53,39 @@ class Program():
         enemy.creature = creature_list.creatures[random.randint(0, len(creature_list.creatures) - 1)]
 
         answer = input("Would you like to start Auto Combat? Enter Y\n")
+        Program.Battle(creature_list, players, enemy, dmg, answer)
+        
+        
+    def Battle(creature_list, players, enemy, dmg, answer):
+        battle = False
         print("You entered: " + answer)
         if answer.lower() == "y":
             battle = True
             if players.creature.speed >= enemy.creature.speed:
                 print("You go first!")
+                print(players.creature.name + " vs " + enemy.creature.name)
                 while battle:
-                    attack_choice = input("Choose an attack: " + ", ".join([attack.name for attack in players.creature.attacks]) + "\n")
-                    attack = next((a for a in players.creature.attacks if a.name.lower() == attack_choice.lower()), None)
-                    if attack is None:
-                        print("Attack not found.")
-                        continue
+                    # When showing the options, number them
+                    attack_list = players.creature.attacks
+                    for i, attack in enumerate(attack_list):
+                        print(str(i + 1) + ". " + attack.name)
+
+                    attack_choice = input("Choose an attack: ")
+
+                    # Check if they typed a number
+                    if attack_choice.isdigit():
+                        index = int(attack_choice) - 1
+                        if 0 <= index < len(attack_list):
+                            attack = attack_list[index]
+                        else:
+                            print("Invalid number.")
+                            attack = None
+                    else:
+                        # Fall back to name matching
+                        attack = next((a for a in attack_list if a.name.lower() == attack_choice.lower()), None)
+                        if attack is None:
+                            print("Attack not found.")
+                            continue
                     damage_dealt = dmg.takeDmg(players.creature, attack, enemy.creature)
                     print("You used " + attack.name + " and dealt " + str(round(damage_dealt, 1)) + " damage!")
                     print("Enemy health: " + str(round(enemy.creature.health, 1)))  # helpful to add this
@@ -75,11 +112,27 @@ class Program():
                         print("You lose!")
                         battle = False
                         break
-                    attack_choice = input("Choose an attack: " + ", ".join([attack.name for attack in players.creature.attacks]) + "\n")
-                    attack = next((a for a in players.creature.attacks if a.name.lower() == attack_choice.lower()), None)
-                    if attack is None:
-                        print("Attack not found.")
-                        continue
+                    # When showing the options, number them
+                    attack_list = players.creature.attacks
+                    for i, attack in enumerate(attack_list):
+                        print(str(i + 1) + ". " + attack.name)
+
+                    attack_choice = input("Choose an attack: ")
+
+                    # Check if they typed a number
+                    if attack_choice.isdigit():
+                        index = int(attack_choice) - 1
+                        if 0 <= index < len(attack_list):
+                            attack = attack_list[index]
+                        else:
+                            print("Invalid number.")
+                            attack = None
+                    else:
+                        # Fall back to name matching
+                        attack = next((a for a in attack_list if a.name.lower() == attack_choice.lower()), None)
+                        if attack is None:
+                            print("Attack not found.")
+                            continue
                     damage_dealt = dmg.takeDmg(players.creature, attack, enemy.creature)
                     print("You used " + attack.name + " and dealt " + str(round(damage_dealt, 1)) + " damage!")
                     print("Enemy health: " + str(round(enemy.creature.health, 1)))  # helpful to add this
